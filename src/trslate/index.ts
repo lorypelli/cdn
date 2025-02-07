c647f1524772c49c9a94b3ad5c72c96f6f28419c
lorypelli <lorypelli@proton.me>
2025-02-07 07:13:41 +0100

    fetch latest if no version specified

--- src/trslate/index.ts
+++ src/trslate/index.ts
@@ -5,13 +5,25 @@ const trslate = new Hono();
 trslate.get('/:version?', async (ctx) => {
     const { version } = ctx.req.param();
     if (!version) {
-        return ctx.json({ error: "Missing package version!" }, 400);
+        const res = await fetch(
+            'https://github.com/lorypelli/trslate/releases/latest',
+        );
+        if (!res.ok) {
+            return ctx.json({ error: res.statusText }, 500);
+        }
+        const v = res.url.split('/').at(-1);
+        if (v) {
+            return ctx.redirect(`/trslate/${v.slice(1)}`, 302);
+        }
+        return ctx.json({ error: 'Missing package version!' }, 400);
     }
-    const res = await fetch(`https://github.com/lorypelli/trslate/releases/download/v${version}/browser.js`);
+    const res = await fetch(
+        `https://github.com/lorypelli/trslate/releases/download/v${version}/browser.js`,
+    );
     if (!res.ok) {
         return ctx.json({ error: res.statusText }, 500);
     }
     return ctx.text(await res.text());
-})
+});
 
 export default trslate;
